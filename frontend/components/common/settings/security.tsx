db09a187a8d6c1950b6a2ccce8c8afca12717f86
6512345 <shionxi65@gmail.com>
2025-12-30 13:44:07 +0800

    fix: cancel in security options does not work

--- frontend/components/common/settings/security.tsx
+++ frontend/components/common/settings/security.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
@@ -58,8 +59,10 @@ export function SecurityMain() {
     }
   }
 
+  const router = useRouter()
+
   const handleCancel = () => {
-    form.reset()
+    router.push("/settings")
   }
 
   return (
