2ee45e5bacb104f0bd8c8ca44d4ddd2b2b099884
maoball <115468859+maoball@users.noreply.github.com>
2026-02-27 10:12:23 +0800

    chore: rename `RedirectURI` to `RedirectURL`

--- frontend/components/common/merchant/merchant-info.tsx
+++ frontend/components/common/merchant/merchant-info.tsx
@@ -115,16 +115,16 @@ export function MerchantInfo({ apiKey, onUpdate, onDelete, updateAPIKey }: Merch
             </Link>
           </div>
 
-          {apiKey.redirect_uri && (
+          {apiKey.redirect_url && (
             <div className="px-3 py-2 flex items-center justify-between border-b border-dashed last:border-b-0">
-              <label className="text-xs font-medium text-muted-foreground">回调 URI</label>
+              <label className="text-xs font-medium text-muted-foreground">回调 URL</label>
               <Link
-                href={apiKey.redirect_uri}
+                href={apiKey.redirect_url}
                 target="_blank"
                 className="text-xs text-indigo-500 hover:underline flex items-center gap-1 text-right max-w-[70%]"
               >
                 <span className="truncate flex-1 min-w-0">
-                  {apiKey.redirect_uri}
+                  {apiKey.redirect_url}
                 </span>
                 <ExternalLink className="size-3 flex-shrink-0 ml-1" />
               </Link>
